d79900668ab0aa23e9fda9f5d447397307db64e3
Manasa-Reddy-C <manasac2404@gmail.com>
2021-04-30 17:24:58 +0530

    uploading

---
+++ theblog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView,DetailView
+
+#def home(request):
+    #return render(request, 'home.html',{})
+class HomeView(ListView):
+    model=Post
+    template_name='home.html'
+class ArticleDetailView(DetailView):
+    model=Post
+    template_name='details.html'
+    
